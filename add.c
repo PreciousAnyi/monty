db48367d8a2a6089129586ffdf4f06d37e418626
preciousanyi <jkpresh@gmail.com>
2024-01-18 23:55:00 +0100

    Fixed memory leaks

--- add.c
+++ add.c
@@ -6,21 +6,11 @@
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp;
-	int add;
-
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	temp = (*stack)->next;
-	if (temp->next == NULL)
-	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	add = (((*stack)->n) + (temp->n));
-	temp->n = add;
-	*stack = temp;
+	(*stack)->next->n += (*stack)->n;
+	pop(stack, line_number);
 }
