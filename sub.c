580e7cbe51387b76b30b11f2d6f8900c46f34b2a
preciousanyi <jkpresh@gmail.com>
2024-01-19 01:15:15 +0100

    Betty fix

--- sub.c
+++ sub.c
@@ -7,15 +7,11 @@
  */
 void sub(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp;
-
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	(*stack)->next->n -= (*stack)->n;
-	temp = (*stack)->next;
 	pop(stack, line_number);
-	free(temp);
 }
